20598a79f17309ac14357145c921bb29f7e7ea6b
David Garcia <byteswright@isdavid.com>
2024-07-17 11:13:13 -0600

    config gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base:"https://github.com/bytesWright/visionCardDocs",
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
